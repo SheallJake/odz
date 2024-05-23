c59295bd2673ea4568fec6230df15a5897118a9c
SheallJake <mr.galaxy2325@gmail.com>
2024-05-23 06:19:49 +0300

    aded playlist title

--- src/components/FilmPlaylist.js
+++ src/components/FilmPlaylist.js
@@ -7,12 +7,13 @@ function FilmPlaylist({ playlist, removeFromPlaylist }) {
 
     return (
         <div>
+            <h1 className='list-title'>Playlist</h1>
             <TransitionGroup className={`film-list`}>
                 {playlist.length > 0 ? playlist.map(movie => (
                     <CSSTransition key={'anim' + movie.id} timeout={200} classNames="fade">
                         <Film key={movie.id} movie={movie} inPlaylist={true} removeFromPlaylist={removeFromPlaylist} playlist={playlist} />
                     </CSSTransition>
-                )) : <h1 className='playlist-empty-title'>Playlist is empty</h1>}
+                )) : <h2 className='playlist-empty-title'>Playlist is empty</h2>}
             </TransitionGroup>
         </div>
     );
